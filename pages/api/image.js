30964cfd30158b40dbce75ddefef6a20c5d8abd3
Dom Eccleston <domeccleston@gmail.com>
2022-12-07 11:30:32 +0000

    use api route

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,17 +1,12 @@
-const QSTASH = `https://qstash.upstash.io/v1/publish/`;
-const DALL_E = "https://api.openai.com/v1/images/generations";
-const VERCEL_URL = "https://dalle-2.vercel.app";
-
 export default async function handler(req, res) {
   const { prompt } = req.query;
+
   try {
-    const response = await fetch(`${QSTASH + DALL_E}`, {
+    const response = await fetch("https://openai-queue.vercel.app/api/images", {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${process.env.QSTASH_TOKEN}`,
-        "upstash-forward-Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
-        "Content-Type": "application/json",
-        "Upstash-Callback": `${VERCEL_URL}/api/callback`,
+        "x-api-route": "/api/callback",
+        Authorization: process.env.OPENAI_API_KEY,
       },
       body: JSON.stringify({
         prompt,
@@ -19,11 +14,10 @@ export default async function handler(req, res) {
         size: "1024x1024",
       }),
     });
-    const json = await response.json();
-    return res.status(202).json({ id: json.messageId });
+    return res.status(200).json(response);
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: error.message, type: "Internal server error" });
+    return res.status(500).json({
+      message: `Error forwarding request to https://openai-queue.vercel.app/: ${error}`,
+    });
   }
 }
