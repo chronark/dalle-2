948daccd7fb632251720e9fe6586ce191cdbef83
Dom Eccleston <domeccleston@gmail.com>
2022-12-07 11:43:52 +0000

    bearer

--- pages/api/image.js
+++ pages/api/image.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "x-api-route": "/api/callback",
-        Authorization: process.env.OPENAI_API_KEY,
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY`,
       },
       body: JSON.stringify({
         prompt,
