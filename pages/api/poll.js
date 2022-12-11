9c7603855d85c15a6926ac5caa7a60e4ca6a35ca
Dom <domeccleston@gmail.com>
2022-12-11 13:39:32 +0000

    env

--- pages/api/poll.js
+++ pages/api/poll.js
@@ -6,6 +6,7 @@ export default async function handler(req, res) {
   const { id } = req.query;
   try {
     console.log({ id, redis });
+    console.log(process.env);
     const data = await redis.get(id);
     if (!data) return res.status(404).json({ message: "No data found" });
     else return res.status(200).json(data);
