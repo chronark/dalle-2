8ab4c12bd1192042e94ad45c71fabc1a02f7d52d
Dom <domeccleston@gmail.com>
2022-12-11 12:42:09 +0000

    switch to netlify

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,17 +1,17 @@
-const QSTASH = `https://qstash.upstash.io/v1/publish/`;
 const DALL_E = "https://api.openai.com/v1/images/generations";
-const VERCEL_URL = "https://dalle-2.vercel.app";
+
+export const config = {
+  type: 'experimental-background'
+}
 
 export default async function handler(req, res) {
   const { prompt } = req.query;
   try {
-    const response = await fetch(`${QSTASH + DALL_E}`, {
+    const response = await fetch(DALL_E, {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${process.env.QSTASH_TOKEN}`,
-        "upstash-forward-Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
         "Content-Type": "application/json",
-        "Upstash-Callback": `${VERCEL_URL}/api/callback`,
       },
       body: JSON.stringify({
         prompt,
@@ -19,8 +19,17 @@ export default async function handler(req, res) {
         size: "1024x1024",
       }),
     });
+
     const json = await response.json();
-    return res.status(202).json({ id: json.messageId });
+  
+    await fetch("/api/callback", { 
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: json
+    })
+  
   } catch (error) {
     res
       .status(500)
