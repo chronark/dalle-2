f840acda62937d0aa6eacb6bf42d7d7228990be0
Dom <domeccleston@gmail.com>
2022-12-11 18:05:20 +0000

    start implementing edge approach

--- pages/api/edge.js
+++ pages/api/edge.js
@@ -1,14 +1,36 @@
+const DALL_E = "https://api.openai.com/v1/images/generations";
+
 export default async function handler(req, res) {
     const start = Date.now();
     const prompt = req.nextUrl.searchParams.get('prompt');
-    return new Response(
-      new ReadableStream({
+    const image = await fetch(DALL_E, {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt,
+          n: 1,
+          size: "1024x1024",
+        }),
+      });
+
+    const json = await image.json();
+
+    const stream = new ReadableStream({
         start(controller) {
           controller.enqueue(new TextEncoder().encode(`Waiting...\n`));
           setInterval(() => {
             const snapshot = new TextEncoder().encode(`${Date.now() - start}\n`)
 
-            console.log(snapshot);
+            console.log({ json });
+            
+            if (!json) {
+                console.log('no data');
+            } else {
+                console.log(json.data[0].url);
+            }
 
             controller.enqueue(snapshot);
   
@@ -18,7 +40,10 @@ export default async function handler(req, res) {
             }
           }, 1000);
         },
-      }),
+      })
+
+    return new Response(
+     "test",
       {
         headers: {
           "content-type": "text/plain",
